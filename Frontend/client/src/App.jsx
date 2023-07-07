5ceb97e6b154b9c6e20e373e1068f6559ec10565
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-06 22:21:37 -0500

    Create App.jsx

---
+++ Frontend/client/src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import TaskForm from './pages/TaskForm'
+import Navbar from './components/NavBar'
+function App(){
+  return(
+    <BrowserRouter>
+      <div className='container mx-auto px-10'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/tasks/new' element={<TaskForm/>}/>
+          <Route path='/tasks/:id' element={<TaskForm/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+export default App
