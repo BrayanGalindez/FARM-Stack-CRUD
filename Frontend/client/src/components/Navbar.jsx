f230233b61101a07a967fd414479828f33174ca6
BrayanGalindez <bgalindez@unal.edu.co>
2023-07-13 18:18:00 -0500

    Update Navbar.jsx

--- Frontend/client/src/components/Navbar.jsx
+++ Frontend/client/src/components/Navbar.jsx
@@ -4,11 +4,11 @@ function Navbar(){
     return(
         <header className='flex justify-between items-center my-7'>
             <Link to="/">
-                <h1 className='text-3xl font-bold'>Task App</h1>
+                <h1 className='text-3xl font-bold'>Mis notas</h1>
             </Link>
 
             <Link to="/tasks/new" className='bg-zinc-950 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded'>
-                <h1 className='text-3xl font-bold'>Create Task</h1>
+                <h1 className='text-3xl font-bold'>Añadir tarea</h1>
             </Link>
         </header>
     );
